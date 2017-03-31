a819c465f1bb5023722155050710c15e3b0b1b85
Muhammad Jawad Akbar <mja30@itl188.student.eecs.qmul.ac.uk>
2017-03-31 14:36:04 +0100

    nav add

--- wsgi/app/views.py
+++ wsgi/app/views.py
@@ -11,7 +11,7 @@ nav = Navigation(app)
 nav.Bar('top', [
 nav.Item('Home', 'index'),
 nav.Item('Emotion App','emotion'),
-nav.Item('Visualization App','visualize_test')
+nav.Item('Visualization App','embed')
 ])
 
 @app.route('/')
@@ -42,36 +42,4 @@ def emotion_post():
 	)
 	return render_template("my_form.html",mood=response.body['result']['sentiment'],form=MessageForm())
 
-@app.route("/visualize_test")
-def polynomial():
-    """ Very simple embedding of a polynomial chart
-    """
 
-    # Grab the inputs arguments from the URL
-    args = flask.request.args
-
-    # Get all the form arguments in the url with defaults
-    color = getitem(args, 'color', 'Black')
-    _from = int(getitem(args, '_from', 0))
-    to = int(getitem(args, 'to', 10))
-
-    # Create a polynomial line graph with those arguments
-    x = list(range(_from, to + 1))
-    fig = figure(title="Polynomial")
-    fig.line(x, [i ** 2 for i in x], color=colors[color], line_width=2)
-
-    js_resources = INLINE.render_js()
-    css_resources = INLINE.render_css()
-
-    script, div = components(fig)
-    html = flask.render_template(
-        'embed.html',
-        plot_script=script,
-        plot_div=div,
-        js_resources=js_resources,
-        css_resources=css_resources,
-        color=color,
-        _from=_from,
-        to=to
-    )
-    return encode_utf8(html)
